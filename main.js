c47091d6421458eb66034cb5b11448e537457bc5
vishwas <vishwas101kumar@gmail.com>
2023-02-08 01:17:06 +0530

    Final commit.

--- main.js
+++ main.js
@@ -6,7 +6,59 @@ const error = document.querySelector('.error-message');
 const taskContainer = document.querySelector('.task-list-container');
 const newTask = document.querySelector('.btn--new-task');
 
+const filterMenu = document.querySelector('.filter-menu');
+const allButton = document.querySelector('.all-btn');
+const completedButton = document.querySelector('.completed-btn');
+const uncompletedButton = document.querySelector('.uncompleted-btn');
+
 let lastId;
+
+let filterNumber = localStorage.getItem('filter') || -1;
+
+allButton.addEventListener('click', (e) => {
+    resetFilter(e);
+    filterButtonColor(e);
+    fetchFilterTask(-1);
+});
+
+completedButton.addEventListener('click', (e) => {
+    resetFilter(e);
+    filterButtonColor(e);
+    fetchFilterTask(1);
+});
+
+uncompletedButton.addEventListener('click', (e) => {
+    resetFilter(e);
+    filterButtonColor(e);
+    fetchFilterTask(0);
+});
+
+let resetFilter = (e) => {
+    for (let ele of filterMenu.children) {
+        ele.style.backgroundColor = '#fff';
+        ele.style.borderColor = '#ebebeb';
+    }
+};
+
+let filterButtonColor = (e) => {
+    e.target.style.backgroundColor = 'var(--color-primary)';
+    e.target.style.borderColor = 'var(--color-primary)';
+};
+
+let fetchFilterTask = (status) => {
+    localStorage.setItem('filter', status);
+    if (status === -1) {
+        allButton.style.backgroundColor = 'var(--color-primary)';
+        allButton.style.borderColor = 'var(--color-primary)';
+        createTask(tasks);
+        return;
+    }
+    let compTask = tasks.filter((obj) => {
+        return obj.status === status
+    });
+    createTask(compTask);
+};
+
 
 
 newTask.addEventListener('click', () => {
@@ -38,14 +90,14 @@ let acceptData = () => {
         description: taskDescription.value,
         status: 0
     });
-    createTask();
+    fetchFilterTask(parseInt(localStorage.getItem('filter')));
     localStorage.setItem('data', JSON.stringify(tasks));
     resetForm();
 }
 
-let createTask = () => {
-    if (tasks.length !== 0) {
-        taskContainer.innerHTML = tasks.map((obj) => {
+let createTask = (t = tasks) => {
+    if (t.length !== 0) {
+        taskContainer.innerHTML = t.map((obj) => {
             let { id, title, dueDate, description, status } = obj;
             return `<div class="task-list" id=task-id-${id}>
             <svg class="icon task--status" onclick=changeStatus(${id}) style="fill : ${status === 0 ? 'var(--color-text)' : 'var(--color-primary)'}">
@@ -85,7 +137,8 @@ let createTask = () => {
 
 };
 
-createTask();
+fetchFilterTask(-1);
+
 
 let resetForm = () => {
     taskTitle.value = '';
@@ -105,13 +158,14 @@ let updateTask = (id) => {
 let deleteTask = (id) => {
     let res = tasks.findIndex((obj) => obj.id === id);
     tasks.splice(res, 1);
-    createTask();
+    fetchFilterTask(parseInt(localStorage.getItem('filter')));
     localStorage.setItem('data', JSON.stringify(tasks));
 }
 
 let changeStatus = (id) => {
     let res = tasks.findIndex((obj) => obj.id === id);
-    tasks[res].status = 1;
-    createTask();
+    tasks[res].status = (tasks[res].status === 0) ? 1 : 0;
+    // console.log(typeof );
+    fetchFilterTask(parseInt(localStorage.getItem('filter')));
     localStorage.setItem('data', JSON.stringify(tasks));
 };
